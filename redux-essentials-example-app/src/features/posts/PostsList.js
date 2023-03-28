653a3c6818fafc640fad71f6eb0b9c1880a2e53b
amane <amachonchon@gmail.com>
2023-03-28 16:52:12 +0900

    Reviewed part5

--- redux-essentials-example-app/src/features/posts/PostsList.js
+++ redux-essentials-example-app/src/features/posts/PostsList.js
@@ -7,59 +7,57 @@ import { fetchPosts, selectAllPosts } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 
-
 const PostExcerpt = ({ post }) => {
-    return (
-        <article className="post-excerpt" key={post.id}>
-            <h3>{post.title}</h3>
-            <div>
-                <PostAuthor userId={post.user} />
-                <TimeAgo timestamp={post.date} />
-                <ReactionButtons post={post} />
-            </div>
-            <p className="post-content">{post.content.substring(0, 100)}</p>
-            <Link to={`/posts/${post.id}`} className="button muted-button">
-                View Post
-            </Link>
-        </article>
-    )
+  return (
+    <article className="post-excerpt" key={post.id}>
+      <h3>{post.title}</h3>
+      <div>
+        <PostAuthor userId={post.user} />
+        <TimeAgo timestamp={post.date} />
+        <ReactionButtons post={post} />
+      </div>
+      <p className="post-content">{post.content.substring(0, 100)}</p>
+      <Link to={`/posts/${post.id}`} className="button muted-button">
+        View Post
+      </Link>
+    </article>
+  )
 }
 
-
 export const PostsList = () => {
-    const dispatch = useDispatch()
-    const posts = useSelector(selectAllPosts)
+  const dispatch = useDispatch()
+  const posts = useSelector(selectAllPosts)
 
-    const postStatus = useSelector(state => state.posts.status)
-    const error = useSelector(state => state.posts.error)
+  const postStatus = useSelector((state) => state.posts.status)
+  const error = useSelector((state) => state.posts.error)
 
-    useEffect(() => {
-        if (postStatus === 'idle') {
-            dispatch(fetchPosts())
-        }
-    }, [postStatus, dispatch])
-
-    let content
-
-    if (postStatus === 'loading') {
-        content = <Spinner text="Loading..." />
-    } else if (postStatus === 'succeeded') {
-        // Sort posts in reverse chronological order by datetime string
-        const orderedPosts = posts
-            .slice()
-            .sort((a, b) => b.date.localeCompare(a.date))
-
-        content = orderedPosts.map(post => (
-            <PostExcerpt key={post.id} post={post} />
-        ))
-    } else if (postStatus === 'failed') {
-        content = <div>{error}</div>
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts())
     }
+  }, [postStatus, dispatch])
 
-    return (
-        <section className="posts-list">
-            <h2>Posts</h2>
-            {content}
-        </section>
-    )
+  let content
+
+  if (postStatus === 'loading') {
+    content = <Spinner text="Loading..." />
+  } else if (postStatus === 'succeeded') {
+    // Sort posts in reverse chronological order by datetime string
+    const orderedPosts = posts
+      .slice()
+      .sort((a, b) => b.date.localeCompare(a.date))
+
+    content = orderedPosts.map((post) => (
+      <PostExcerpt key={post.id} post={post} />
+    ))
+  } else if (postStatus === 'failed') {
+    content = <div>{error}</div>
+  }
+
+  return (
+    <section className="posts-list">
+      <h2>Posts</h2>
+      {content}
+    </section>
+  )
 }
--- redux-essentials-example-app/src/features/users/usersSlice.js
+++ redux-essentials-example-app/src/features/users/usersSlice.js
@@ -1,22 +1,22 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { client } from "../../api/client";
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
 const initialState = []
 
 export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
-    const response = await client.get('/fakeApi/users')
-    return response.data
+  const response = await client.get('/fakeApi/users')
+  return response.data
 })
 
 const usersSlice = createSlice({
-    name: 'users',
-    initialState,
-    reducers: {},
-    extraReducers(builder){
-        builder.addCase(fetchUsers.fulfilled, (state, action) => {
-            return action.payload
-        })
-    }
+  name: 'users',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  },
 })
 
 export default usersSlice.reducer
